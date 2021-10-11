45d8634d49041bf0c86a3e6b472cc249a7101aec
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-10-11 15:41:24 +0200

    Update the way moment.js's locale is initialized and updated

--- src/renderer/components/Onboarding/LangSwitcher.js
+++ src/renderer/components/Onboarding/LangSwitcher.js
@@ -111,7 +111,6 @@ const LangSwitcher = () => {
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    moment.locale(language);
     i18n.changeLanguage(language);
   }, [i18n, language]);
 
